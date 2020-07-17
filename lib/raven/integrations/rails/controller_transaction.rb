1a74cc6f34e6cca6f16fa6b8079bc3d7b788a527
t.shinohara <takenari.shinohara@gmail.com>
2020-07-17 16:07:53 +0900

    Rails3系でも動作するように
    controller の prepend_around_action が、rails3では prepend_around_filter なので。
    
    https://github.com/halida/raven-ruby/commit/1910ed9cb7f967e5f3d247c7959fc438c2efe9c7
    を参考に

--- lib/raven/integrations/rails/controller_transaction.rb
+++ lib/raven/integrations/rails/controller_transaction.rb
@@ -2,7 +2,9 @@ module Raven
   class Rails
     module ControllerTransaction
       def self.included(base)
-        base.prepend_around_action do |controller, block|
+        filter_method_name = (::Rails::VERSION::MAJOR == 3) ? :prepend_around_filter : :prepend_around_action
+        filter_method = base.method(filter_method_name)
+        filter_method.call do |controller, block|
           Raven.context.transaction.push "#{controller.class}##{controller.action_name}"
           block.call
           Raven.context.transaction.pop
